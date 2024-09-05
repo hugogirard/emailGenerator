2f6ba51620549fc8489d5f4b4e64b0761fe8e302
Hugo Girard <hugirard@microsoft.com>
2024-09-05 14:13:51 -0400

    added gitignore

--- src/api/app/services/openai.service.py
+++ src/api/app/services/openai.service.py
@@ -1,4 +1,6 @@
-
+from dotenv import load_dotenv
+from langchain_openai import AzureChatOpenAI
+import os
 
 class OpenAI:
     _instance = None
@@ -10,4 +12,10 @@ class OpenAI:
 
     def __init__(self):
         if not hasattr(self, 'initialized'):
-          pass
+          load_dotenv()
+          self.chat = AzureChatOpenAI(
+              azure_endpoint=os.getenv('AZURE_OPENAI_ENDPOINT'),
+              azure_deployment=os.getenv('AZURE_OPENAI_DEPLOYMENT'),
+              azure_api_version=os.getenv('AZURE_OPENAI_API_VERSION'),
+              azure_key=os.getenv('AZURE_OPENAI_KEY')
+          )          
